7222af6cfdf535525fbd10e9c35e5657efd01926
waweroh <scadmsez@gmail.com>
2023-06-16 14:21:51 +0300

    add description

--- photos_app/urls.py
+++ photos_app/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('photos_list/', views.photos_list, name='photos_list'),
+    path('', views.photos_list, name='photos_list'),
     path('index/<str:pk>', views.index, name='index'),
 ]
